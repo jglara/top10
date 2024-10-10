cccb24164ed4d7ba3ad1129347fbb3238fc22635
jglara <jglara@gmail.com>
2024-10-10 20:12:05 +0200

    max gap in no linear time

---
+++ 06_max_gap/rust/main.rs
@@ -0,0 +1,27 @@
+use itertools::*;
+
+fn max_gap(xs: Vec<i32>) -> i32 {
+    xs.iter()
+        .sorted()
+        .tuple_windows()
+        .map(|(x, y)| y - x)
+        .max()
+        .unwrap_or_default()
+}
+
+fn main() {}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_1() {
+        assert_eq!(max_gap(vec![3,6,9,1]),3)
+    }
+
+    #[test]
+    fn test_2() {
+        assert_eq!(max_gap(vec![10]),0)
+    }
+}
