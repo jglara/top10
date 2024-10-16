17ee6a9c592dfa9eee3ec3200387d6cb74486c46
jglara <jglara@gmail.com>
2024-10-16 07:25:18 +0200

    08 in rust, branchless solution

--- 08_three_consecutive_odds/rust/main.rs
+++ 08_three_consecutive_odds/rust/main.rs
@@ -2,16 +2,14 @@
 
 fn three_consecutive_odds(arr: Vec<i32>) -> bool {
     arr.iter().map(|x| x % 2).scan(0, |acum, x| {
-        if x == 0 {
-            *acum = 0
-        } else {
-            *acum += 1
-        };
+        *acum += 1;
+        *acum *= x;
         Some(*acum)
     }).any(|x| x == 3)
 }
 
 fn main() {
+    println!("{:?}", three_consecutive_odds(vec![1, 2, 1, 1]));
     println!("{:?}", three_consecutive_odds(vec![2, 6, 4, 1]));
     println!("{:?}", three_consecutive_odds(vec![1,2,34,3,4,5,7,23,12]));
 }
