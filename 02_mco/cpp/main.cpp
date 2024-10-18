d216c4860c557400820a9ba7ecf50ef9872396e7
jglara <jglara@gmail.com>
2024-10-18 14:56:24 +0200

    Max Cum ones cpp (no ranges)

---
+++ 02_mco/cpp/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+
+int findMaxConsecutiveOnes(std::vector<int>& nums) {
+    std::vector<int> cum_ones{};
+    std::inclusive_scan(nums.cbegin(), nums.cend(), std::back_inserter<>(cum_ones), [] (auto acum, auto x) {
+        if (x == 1) {
+            acum++;
+        } else {
+            acum=0;
+        }
+        return acum;
+
+    });
+
+    return *std::max_element(cum_ones.cbegin(), cum_ones.cend());
+}
+
+
+int main()
+{
+    auto v = std::vector<int>{1,1,0,1,1,1};
+    std::cout << findMaxConsecutiveOnes(v) << "\n";
+}
