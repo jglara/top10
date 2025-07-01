5f1c78ba3e745f556c29efb3460b2c3c9a98fe44
jglara <jglara@gmail.com>
2025-07-01 12:57:15 +0200

    Better solution for 02_mco

--- 02_mco/cpp/main.cpp
+++ 02_mco/cpp/main.cpp
@@ -2,6 +2,11 @@
 #include <vector>
 #include <algorithm>
 #include <numeric>
+#include <ranges>
+
+
+namespace rng = std::ranges;
+namespace vws = std::views;
 
 int findMaxConsecutiveOnes(std::vector<int>& nums) {
     std::vector<int> cum_ones{};
@@ -15,8 +20,19 @@ int findMaxConsecutiveOnes(std::vector<int>& nums) {
 }
 
 
+int mco(std::vector<int>& nums) {
+    auto cum = nums | vws::transform([cum_ones = 0] (const auto &x) mutable {
+        cum_ones = (cum_ones+1) * x;
+        return cum_ones;
+    });    
+
+    return rng::fold_left(cum, 0, [] (auto x, auto y) { return std::max(x, y); });
+}
+
+
 int main()
 {
     auto v = std::vector<int>{1,1,0,1,1,1};
     std::cout << findMaxConsecutiveOnes(v) << "\n";
+    std::cout << mco(v) << "\n";
 }
